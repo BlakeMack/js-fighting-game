43aba84b01e8005d1c0230b7edb300d1382c78d2
Blake McKenzie <blakemckenzie365@gmail.com>
2022-10-02 19:48:21 +0100

    typed.js installed via npm, typed.js script tag added to html, attempt to use type.js in utilities.js, but Typed instance not recognized within utilities.js currently

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,11 @@
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
+
 function rectangularCollision({ rectangle1,rectangle2 }) {
   return (
     rectangle1.attackbox.position.x + rectangle1.attackbox.width >= rectangle2.position.x &&
@@ -5,7 +13,7 @@ function rectangularCollision({ rectangle1,rectangle2 }) {
     rectangle1.attackbox.position.y + rectangle1.attackbox.height >= rectangle2.position.y &&
     rectangle1.attackbox.position.y <= rectangle2.position.y + rectangle2.height
     )
-}
+  }
 
 function determineWinner({player, enemy, timerId}) {
   clearTimeout(timerId)
@@ -14,10 +22,14 @@ function determineWinner({player, enemy, timerId}) {
   document.querySelector("#DisplayText").style.display = 'flex'
   if (player.health === enemy.health) {
     console.log("tie")
+    // where message is passed through to html
     document.querySelector("#DisplayText").innerHTML = 'Tie'
   } else if (player.health > enemy.health ) {
+    // where message is passed through to html
+    console.log(typed)
     document.querySelector("#DisplayText").innerHTML = 'Player 1 Wins'
   } else if (enemy.health > player.health ) {
+    // where message is passed through to html
     document.querySelector("#DisplayText").innerHTML = 'Player 2 Wins'
   }
 }
